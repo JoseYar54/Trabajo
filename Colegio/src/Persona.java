323d6451edcd2875e7c3b44c7b936ca23c479b09
Jordy <joseyar1032@gmail.com>
2024-10-25 15:49:23 -0500

    Prueba

--- Colegio/src/Persona.java
+++ Colegio/src/Persona.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author User
- */
 public class Persona {
-    
+    private int edad;
+    private String nombre;
+    private String ape_materno;
+    private String ape_paterno;
 }
